1233d244dc40ae1f69f50d8d8820dfbdca0d79bf
Paulo <aguiarlapaulo@gmail.com>
2025-08-21 12:52:14 -0300

    feat:bitcoinapi

--- api_bitcoin.py
+++ api_bitcoin.py
@@ -1,6 +1,24 @@
 import requests
+from datetime import datetime
+import pandas as pd
 
 url= 'https://api.coinbase.com/v2/prices/spot'
 response = requests.get(url)
 data = response.json()
 print(data)
+
+#Extract the data
+price = data['data']['amount']
+currency = data['data']['currency']
+ticker = data['data']['base']
+timestamp = datetime.now()
+
+# Create df
+df = pd.DataFrame([{
+    'ticker': ticker,
+    'price': price,
+    'currency': currency,
+    'timestamp': timestamp
+}])
+
+print(df)
